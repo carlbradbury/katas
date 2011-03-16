b9a122229f0b83886be52eeadfea2fcb2af6643a
unknown <carl@.(none)>
2011-03-16 23:40:32 +0000

    Pushing new changes

---
+++ product/katas.specs/PrettyPathPrint/PrettyPathPrinterSpecs.cs
@@ -0,0 +1,57 @@
+﻿
+using System;
+using System.IO;
+using developwithpassion.specifications.rhino;
+using katas.PrettyPathPrint;
+using Machine.Specifications;
+
+namespace katas.specs.PrettyPathPrint
+{
+    
+    class PrettyPathPrinterSpecs
+    {
+
+        public abstract class concern : Observes<PrettyPathPrinter>
+        {
+//            Establish c = () =>
+//            {
+//                provide_a_basic_sut_constructor_argument(2);
+//            };
+        }
+
+        [Subject(typeof(PrettyPathPrinter))]
+        public class when_printing_path_with_depth_of_one :concern
+        {
+            private Establish e = () =>
+                                      {
+                                          path = @"C:\Program Files";
+                                      };
+
+            Because b = () =>
+                result = sut.print(path);
+
+            It should_contain_one_newline_character = () =>
+                result.ShouldContain(Environment.NewLine);
+
+            static string result;
+            static string path;
+        }
+
+        [Subject(typeof(PrettyPathPrinter))]
+        public class when_path_does_not_exist : concern
+        {
+            private Establish e = () =>
+            {
+                path = @"xxx";
+            };
+            Because b = () =>
+                           catch_exception(() => sut.shut_down());
+
+            It should_throw_a_security_exception = () =>
+                exception_thrown_by_the_sut.ShouldBeAn<ArgumentException>();
+
+            static string result;
+            static string path;
+        }
+    }
+}
---
+++ product/katas/PrettyPathPrint/PrettyPathPrinter.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace katas.PrettyPathPrint
+{
+
+
+    public class PrettyPathPrinter
+    {
+        public static string print(string path)
+        {
+            if (File.Exists(path))
+            {
+                StringBuilder sb = new StringBuilder();
+
+                FileAttributes fas = File.GetAttributes(path);
+                if ((fas & FileAttributes.Directory) == FileAttributes.Directory)
+                {
+                    Console.WriteLine("Given path is directory");
+                }
+                else
+                {
+                    AppendLeaf(ref sb, 0, path);
+                    return sb.ToString();
+                }
+
+
+            }
+            throw new ArgumentException("No such path exists");
+        }
+
+        private static void AppendLeaf(ref StringBuilder sb, int depth, string name)
+        {
+            for (int i = 0; i < depth; i++)
+            {
+                sb.Append("  ");
+            }
+            sb.Append(name).Append(Environment.NewLine);
+        }
+
+    }
+}
