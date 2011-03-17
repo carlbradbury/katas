f99e1175a27daf584dfd24ad61996f2f72e33db8
unknown <carl@.(none)>
2011-03-17 08:52:39 +0000

    Pushing new changes

--- init.ps1
+++ init.ps1
@@ -3,4 +3,4 @@ cp build\local_properties.rb.customize build\local_properties.rb
 cp build\connection_details.rb.customize build\connection_details.rb
 
 "Creating the remote link to developwithpassion code base"
-git remote add jp git://github.com/developwithpassion/uks_feb_2011.git
+git remote add jp git://github.com/developwithpassion/uks_mar_2011.git
